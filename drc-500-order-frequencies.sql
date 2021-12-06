cf50ec04900da777bb3c7b6517b9659d36f9f73d
Asad Rauf <asad.rauf@foodpanda.pk>
2021-12-06 18:31:41 +0500

    update drc 500 to include orders per customer

--- drc-500-order-frequencies.sql
+++ drc-500-order-frequencies.sql
@@ -1,5 +1,7 @@
-select format_date ('Week %W, %Y', created_date_local) as week,
-  count(o.code) as orders
+with data as (select format_date ('%B', created_date_local) as month,
+  v.city,
+  count(distinct pd_customer_uuid) as unique_customers,
+  count(distinct o.code) as orders
 from `fulfillment-dwh-production.pandata_curated.pd_orders` o
 join `dhh---analytics-apac.pandata_pk.pk_accurate_verticals` v
   on v.vendor_code = o.vendor_code
@@ -11,4 +13,8 @@ where o.global_entity_id = 'FP_PK'
   and v.vertical = 'restaurants'
   and o.expedition_type = 'delivery'
   and o.is_own_delivery
-group by 1
+  and v.city in ('Karachi', 'Lahore', 'Islamabad', 'Faisalabad', 'Rawalpindi', 'Multan')
+group by 1,2)
+
+select *, orders/unique_customers as orders_per_customer
+from data
